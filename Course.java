deb8f0dcbbb244b1ad4395e02ee3a10db3c48674
Muhammad Hafiz bin Khairul Kamal <147372834+muhammadhafizkk@users.noreply.github.com>
2026-06-19 16:17:18 +0800

    exercise 2

--- Course.java
+++ Course.java
@@ -3,13 +3,17 @@ public class Course {
     private String title;
     private int durationHours;
     private String level;
+    private String category;
+    private boolean active;
     private Instructor instructor;
     
-    public Course(String courseId, String title, int durationHours, String level) {
+    public Course(String courseId, String title, int durationHours, String level, String category, boolean active) {
         this.courseId = courseId;
         this.title = title;
         this.durationHours = durationHours;
         this.level = level;
+        this.category = category;
+        this.active = active;
     }
 
     public String getCourseId() {
@@ -53,10 +57,18 @@ public class Course {
     }
 
     public void printSummary() {
+        System.out.println("=".repeat(40));
         System.out.println("Course ID: " + courseId);
         System.out.println("Title: " + title);
         System.out.println("Duration: " + durationHours);
         System.out.println("Level: " + level);
+        System.out.println("Category: " + category);
+
+        if (active == true) {
+            System.out.println("Status: Active");
+        } else {
+            System.out.println("Status: Inactive");
+        }
 
         if (instructor == null) {
             System.out.println("Instructor: Not yet assigned");
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Course course1 = new Course("CS001", "Introduction to Computer Science", 40, "Beginner");
-        Course course2 = new Course("CS002", "React Frontend Development", 21, "Intermediate");
+        //Instructor intructor1 = new Instructor("I001", "James Bond", "Artificial Intelligence");
+        //Instructor intructor2 = new Instructor("I002", "Benoit Blanc", "Cybersecurity");
+        
+        Course course1 = new Course("CS001", "Introduction to Computer Science", 40, "Beginner", "Fullstack Programming", true);
+        Course course2 = new Course("CS002", "React Frontend Development", 21, "Intermediate", "Frontend Developmenet", false);
+        
         course1.printSummary();
         course2.printSummary();
     }
