11cb001ab633d57385dadb1ff5d7cba70a9e260e
Muhammad Hafiz bin Khairul Kamal <147372834+muhammadhafizkk@users.noreply.github.com>
2026-06-27 10:11:50 +0800

    day 4 - exercise 1

---
+++ js/javascript-demo.js
@@ -0,0 +1,14 @@
+const course = {
+    courseId: "C001",
+    title: "Javscript Fundamentals",
+    durationHours: 12,
+    level: "Beginner",
+    instructor: "John Doe"
+}
+
+console.log("=== Course Details ===")
+console.log(`Course ID: ${course.courseId}`)
+console.log(`Title: ${course.title}`)
+console.log(`Duration: ${course.durationHours}`)
+console.log(`Level: ${course.level}`)
+console.log(`Instructor: ${course.instructor}`)
---
+++ js/student-object.js
@@ -0,0 +1,13 @@
+const student = {
+    studentId: "S001",
+    studentName: "Ignacio de Paul",
+    email: "ignacio@example.com",
+    status: "Active"
+}
+
+console.log("=== Student Object ===")
+console.log(student)
+console.log(student.studentId)
+console.log(student.studentName)
+console.log(student["email"])
+console.log(student["status"])
